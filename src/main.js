2cd9f013e7cd46c3ee5b3f0442478d5f0a12dea7
绝恋。。 <1105033640@qq.com>
2023-08-13 21:53:24 +0800

    pinia独立main.js文件统一管理，类似vuex。store统一导出。

--- src/main.js
+++ src/main.js
@@ -1,15 +1,12 @@
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import persist from 'pinia-plugin-persistedstate'
+
 import App from './App.vue'
 import router from './router'
 
 import '@/assets/main.scss'
+import pinia from '@/stores'
 
 const app = createApp(App)
-const pinia = createPinia()
-
-pinia.use(persist)
 
 app.use(pinia)
 app.use(router)
