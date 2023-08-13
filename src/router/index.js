2611dddcafd9f481159608a3e317f1085d599cf7
绝恋。。 <1105033640@qq.com>
2023-08-13 17:55:05 +0800

    Vue3中使用vue-router4语法与在Vue2中使用vue-router3的差异

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,18 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import {
+  createRouter,
+  createWebHistory,
+  createWebHashHistory
+} from 'vue-router'
 
 const router = createRouter({
+  // history： 用于指定路由模式，
+  //    createWebHistory  就是history模式
+  //    createWebHashHistory  就是hash模式
+
+  // vite 的配置 import.meta.env.BASE_URL 是路由的基准地址，默认是 ’/‘
+  // 参考vite官方说明：https://vitejs.dev/guide/build.html#public-base-path
+  // 如果将来你部署的域名路径是：http://xxx/my-path/user
+  // 在 vite.config.ts 添加配置  base: my-path，，那么路由就会加上 my-path 前缀了
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     // {
