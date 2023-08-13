7ff25e4b0bc478a9a913c07e4a0e794f6f0bd538
绝恋。。 <1105033640@qq.com>
2023-08-13 17:48:46 +0800

    项目初始化文件添加

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,9 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-</script>
+<script setup></script>
 
 <template>
-  <div class="wrapper">
-    <nav>
-      <RouterLink to="/">Home</RouterLink>
-      <RouterLink to="/about">About</RouterLink>
-    </nav>
+  <div>
+    <router-view></router-view>
   </div>
-
-  <RouterView />
 </template>
 
 <style scoped></style>
