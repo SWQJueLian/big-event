de5ea4621a764f5422e7cd6f53c63442075e136f
绝恋。。 <1105033640@qq.com>
2023-08-14 11:28:54 +0800

    登录页面表单校验实现

--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -1,30 +1,86 @@
 <script setup>
 import { User, Lock, Key } from '@element-plus/icons-vue'
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
 
 const isRegister = ref(true)
+
+// 整个from表单的绑定数据对象
+const fromModel = reactive({
+  username: '',
+  password: '',
+  repassword: ''
+})
+// 表单的校验规则
+// 更多高级校验规则用法：https://github.com/yiminghe/async-validator
+const fromRules = reactive({
+  username: [
+    // 非空校验， required: true
+    { required: true, message: '请输入用户名', trigger: 'blur' },
+    // 长度校验
+    { min: 5, max: 20, message: '用户名长度5-20位', trigger: 'blur' }
+  ],
+  password: [
+    { required: true, message: '请输入密码', trigger: 'blur' },
+    // 正则校验
+    { pattern: /^\S{6,20}$/, message: '密码位6-20位非空字符', trigger: 'blur' }
+  ],
+  repassword: [
+    { required: true, message: '请输入确认密码', trigger: 'blur' },
+    // 正则校验
+    {
+      pattern: /^\S{6,20}$/,
+      message: '密码为6-20位非空字符',
+      trigger: 'blur'
+    },
+    {
+      validator: (rule, value, callback) => {
+        // rule: 当前校验规则
+        // value: 当前表单的值
+        // callback： 回调函数，不管是否校验成功，都必须调用callback
+        if (value !== fromModel.password) {
+          callback(new Error('两次输入的密码不一致！'))
+        } else {
+          callback()
+        }
+      }
+    }
+  ]
+})
 </script>
 
 <template>
   <el-row class="login-page">
     <el-col :span="10" class="bg"></el-col>
     <el-col :span="6" :offset="4" class="form">
-      <el-form ref="form" size="large" autocomplete="off" v-if="isRegister">
+      <el-form
+        :model="fromModel"
+        :rules="fromRules"
+        ref="form"
+        size="large"
+        autocomplete="off"
+        v-if="isRegister"
+      >
         <el-form-item>
           <h1>注册</h1>
         </el-form-item>
-        <el-form-item>
-          <el-input :prefix-icon="User" placeholder="请输入用户名"></el-input>
-        </el-form-item>
-        <el-form-item>
+        <el-form-item prop="username">
           <el-input
+            v-model="fromModel.username"
+            :prefix-icon="User"
+            placeholder="请输入用户名"
+          ></el-input>
+        </el-form-item>
+        <el-form-item prop="password">
+          <el-input
+            v-model="fromModel.password"
             :prefix-icon="Key"
             type="password"
             placeholder="请输入密码"
           ></el-input>
         </el-form-item>
-        <el-form-item>
+        <el-form-item prop="repassword">
           <el-input
+            v-model="fromModel.repassword"
             :prefix-icon="Key"
             type="password"
             placeholder="请输入再次密码"
