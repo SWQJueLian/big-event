43b233b149b4aea6e1e73fe1a229b99402096477
绝恋。。 <1105033640@qq.com>
2023-08-14 12:06:22 +0800

    自动导入和手动导入冲突后css样式加载不了的BUG修复（不手动导入就没有语法提示，难受呀...）[NEW]

--- vite.config.js
+++ vite.config.js
@@ -5,16 +5,32 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import {
+  createStyleImportPlugin,
+  ElementPlusResolve
+} from 'vite-plugin-style-import'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     AutoImport({
-      resolvers: [ElementPlusResolver({ importStyle: false })]
+      resolvers: [ElementPlusResolver()]
     }),
     Components({
-      resolvers: [ElementPlusResolver({ importStyle: false })]
+      resolvers: [ElementPlusResolver()]
+    }),
+    createStyleImportPlugin({
+      resolves: [ElementPlusResolve()],
+      libs: [
+        {
+          libraryName: 'element-plus',
+          esModule: true,
+          resolveStyle: (name) => {
+            return `element-plus/theme-chalk/${name}.css`
+          }
+        }
+      ]
     })
   ],
   resolve: {
