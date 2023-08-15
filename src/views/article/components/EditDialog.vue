978d10205d9870bc8b537d85793fa1b31f98195d
绝恋。。 <1105033640@qq.com>
2023-08-15 09:59:15 +0800

    EditDialog添加表单校验规则

--- src/views/article/components/EditDialog.vue
+++ src/views/article/components/EditDialog.vue
@@ -6,16 +6,37 @@ import {
 } from '@/apis/article'
 import { ElMessage } from 'element-plus'
 
+// 是否显示dialog框
 const dialogVisible = ref(false)
 
+// 自定义标题
 const custtitle = ref('')
 
+// 表单绑定数据对象
 const rowData = ref({
   id: '',
   cate_name: '',
   cate_alias: ''
 })
 
+// 表单校验规则
+const fromRules = {
+  cate_name: [
+    { required: true, message: '请输入分类名称', tirgger: 'blur' },
+    { pattern: /^\S{4,20}$/, message: '请输入4-20位非空字符', tirgger: 'blur' }
+  ],
+
+  cate_alias: [
+    { required: true, message: '请输入分类别名', tirgger: 'blur' },
+    {
+      pattern: /^[a-zA-Z0-9]{2,20}$/,
+      message: '请输入2-15位字母或数字字符',
+      tirgger: 'blur'
+    }
+  ]
+}
+
+// 对外exposed的方法，用于父组件调用并显示对话框
 const openDialog = (title, data) => {
   custtitle.value = title
   dialogVisible.value = true
@@ -24,6 +45,7 @@ const openDialog = (title, data) => {
 
 const emits = defineEmits(['updateData'])
 
+// 点击“确定”按钮处理函数
 const handlerConfirm = async () => {
   // 有ID就代表是编辑模式，没有就是添加
   if (rowData.value.id) {
@@ -44,6 +66,7 @@ const handlerConfirm = async () => {
   emits('updateData')
 }
 
+// 对外暴露定义
 defineExpose({
   openDialog
 })
@@ -53,11 +76,11 @@ defineExpose({
   <el-dialog v-model="dialogVisible" :title="custtitle" width="30%">
     <template #default>
       {{ rowData }}
-      <el-form :model="rowData" label-width="100px">
-        <el-form-item label="分类名称：">
+      <el-form :model="rowData" :rules="fromRules" label-width="100px">
+        <el-form-item label="分类名称：" prop="cate_name">
           <el-input v-model="rowData.cate_name" />
         </el-form-item>
-        <el-form-item label="分类别名：">
+        <el-form-item label="分类别名：" prop="cate_alias">
           <el-input v-model="rowData.cate_alias" />
         </el-form-item>
       </el-form>
